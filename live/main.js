76e269748b1a667ff6aa3a6cf6b3b4f6e635a48a
cleddrence <cydmdalupan@gmail.com>
2025-11-26 10:15:10 +0000

    merge

--- live/main.js
+++ live/main.js
@@ -31732,7 +31732,7 @@ var routes = [
   { path: "", component: ChatComponent },
   {
     path: "admin",
-    loadChildren: () => import("./chunk-TMJJW6VL.js").then((m) => m.AdminModule)
+    loadChildren: () => import("./chunk-R2NMQMFK.js").then((m) => m.AdminModule)
   }
 ];
 
