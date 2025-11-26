326dac8e1146c5da82715f3aac37fbfc8f8ad308
cyd <cydmdalupan@gmail.com>
2025-11-27 00:12:23 +0800

    build android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.welfaresuper.app',
   appName: 'WelfareSuper',
-  webDir: 'www'
+  webDir: 'live'
 };
 
 export default config;
