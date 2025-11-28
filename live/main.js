fd229eb2dc854c2ef7862509a5010e4e44a3bec4
cleddrence <cydmdalupan@gmail.com>
2025-11-28 02:21:25 +0000

    chat page

--- live/main.js
+++ live/main.js
@@ -31775,7 +31775,7 @@ var routes = [
   { path: "", component: ChatComponent },
   {
     path: "admin",
-    loadChildren: () => import("./chunk-ZB75HGRV.js").then((m) => m.AdminModule)
+    loadChildren: () => import("./chunk-BHWYSDUY.js").then((m) => m.AdminModule)
   }
 ];
 
